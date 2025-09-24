8f00664c3d65bc6de4078ab33f88814bb3a876e2
Yusra Aman <Yusra.aman00@gmail.com>
2025-09-24 18:34:09 +0200

    "css imported"

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import HomePage from "./pages/HomePage";
+import "./App.css"; 
 
 function App() {
-  return <HomePage />;
+  return (
+    <div className="app">
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
