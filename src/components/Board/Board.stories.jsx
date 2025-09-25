29fcc58fdcf4536c79e6aec452e7c8f4b7e81217
Yusra Aman <Yusra.aman00@gmail.com>
2025-09-25 17:55:06 +0200

    updated stories file

--- src/components/Board/Board.stories.jsx
+++ src/components/Board/Board.stories.jsx
@@ -1,8 +1,18 @@
-import { Board } from ".";
+import React from "react";
+import Board from "./Board";
+import { GameProvider } from "../../context/GameContext";
+import { BrowserRouter } from "react-router-dom";
+
 
 export default {
   title: "Components/Board",
   component: Board,
 };
 
-export const Default = {};
+export const Default = () => (
+  <BrowserRouter>
+    <GameProvider>
+      <Board />
+    </GameProvider>
+  </BrowserRouter>
+);
